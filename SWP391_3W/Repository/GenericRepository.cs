5e6969c4fa792d398ca1adf45c98848c0ed42f8e
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-11 14:41:59 +0700

    add GenericRepository

---
+++ SWP391_3W/Repository/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using BusinessObj.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DASContext _context = null;
+        protected readonly DbSet<T> _db = null;
+        public void Add(T entity)
+        {
+            _db.Add(entity);
+        }
+
+        public void Delete(object id)
+        {
+            T exist = _db.Find(id);
+            if (exist != null)
+            {
+                _db.Remove(exist);
+            }
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _db.ToList();
+        }
+
+        public T GetByID(object id)
+        {
+            return _db.Find(id);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _db.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
