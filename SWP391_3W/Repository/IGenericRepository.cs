52296eb0747dc04f6d88b6fdb52ee4abc2e29074
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-11 14:36:31 +0700

    push IGenericRepository

---
+++ SWP391_3W/Repository/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T GetByID(Object id);
+        void Save();
+        void Delete(object id);
+        void Add(T entity);
+        void Update(T entity);
+        IEnumerable<T> GetAll();
+    }
+}
