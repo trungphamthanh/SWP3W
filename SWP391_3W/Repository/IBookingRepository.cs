5bcb782f6086d48fd199d23e1d978a0976ed684c
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-11 15:07:53 +0700

    add IBookingRepository

---
+++ SWP391_3W/Repository/IBookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IBookingRepository
+    {
+        public IEnumerable<Booking> GetBookingsByCustomerName(string customerName);
+    }
+}
