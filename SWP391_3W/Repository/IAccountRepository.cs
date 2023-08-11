957a18980ec0bf7c5029f83370b1cc894cce984d
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-11 14:58:42 +0700

    add IAccountRepository

---
+++ SWP391_3W/Repository/IAccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IAccountRepository
+    {
+        public Account GetAccount(string username, string password);
+    }
+}
