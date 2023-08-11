cfb89b7c7a70d831b949a61353bb9dfe91007b35
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-11 15:19:54 +0700

    add Program

---
+++ SWP391_3W/BookingWebs/Program.cs
@@ -0,0 +1,31 @@
+using BusinessObj.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<DASContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
