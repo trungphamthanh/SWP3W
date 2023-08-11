e7ce5ee64cb86c90c54ccb911c23e2fe10f6b7f0
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-11 15:02:57 +0700

    add AccountRepository

---
+++ SWP391_3W/Repository/AccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using BusinessObj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AccountRepository : GenericRepository<Account>, IAccountRepository
+    {
+        public Account GetAccount(string username, string password)
+        {
+            return _context.Accounts.FirstOrDefault(s => s.Username.Equals(username) && s.Password.Equals(password));
+        }
+    }
+}
