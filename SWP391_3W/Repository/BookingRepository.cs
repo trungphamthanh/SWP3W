2fbb215cbb64a4f2b3cd2d1900176ebed029b870
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-11 15:14:51 +0700

    add BookingRepository

---
+++ SWP391_3W/Repository/BookingRepository.cs
@@ -0,0 +1,17 @@
+﻿using BusinessObj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class BookingRepository : GenericRepository<Booking>, IBookingRepository
+    {
+        public IEnumerable<Booking> GetBookingsByCustomerName(string customerName)
+        {
+            return _context.Bookings.Where(s => s.CustomerName.Contains(customerName)).ToList();
+        }
+    }
+}
