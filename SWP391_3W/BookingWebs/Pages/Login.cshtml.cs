9583c50af221c8a88095934ce060766c4514e71f
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-14 14:49:38 +0700

    CRUD

---
+++ SWP391_3W/BookingWebs/Pages/Login.cshtml.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repositories;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookingWebs.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public readonly IAccountRepository _accountRepository;
+        public LoginModel(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPostAsync(string username, string password)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var account = _accountRepository.GetAccount(username, password);
+            if (account == null)
+            {
+                ViewData["Title"] = "Incorrect username or password";
+                return Page();
+            }
+            else
+            {
+                if (account.RoleId == 1)
+                {
+                    return RedirectToPage("/Service/Index");
+                }
+                if (account.RoleId == 2)
+                {
+                    return Page();
+                }
+                if (account.RoleId == 3)
+                {
+                    return Page();
+                }
+            }
+            return Page();
+        }
+    }
+}
---
+++ SWP391_3W/BookingWebs/Pages/Service/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObj.Model;
+
+namespace BookingWebs.Pages.Service
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BusinessObj.Model.DASContext _context;
+
+        public IndexModel(BusinessObj.Model.DASContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Daservice> Daservice { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Daservices != null)
+            {
+                Daservice = await _context.Daservices
+                .Include(d => d.Account).ToListAsync();
+            }
+        }
+    }
+}
