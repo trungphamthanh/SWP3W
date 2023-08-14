7c28440fe22053a99659be36281fbd5c323b1ded
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-14 10:02:43 +0700

    update IAccountRepository và AccountRepository

---
+++ SWP391_3W/Repositories/AccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using BusinessObj.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class AccountRepository : GenericRepository<Account>, IAccountRepository
+    {
+        public Account GetAccount(string username, string password)
+        {
+            return _context.Accounts.FirstOrDefault(s => s.Username.Equals(username) && s.Password.Equals(password));
+        }
+    }
+}
