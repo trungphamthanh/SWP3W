d046b3586fe0cfdc7a1e0b78d1dcb342ddfa1793
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-14 11:02:15 +0700

    upload IServiceRepository

---
+++ SWP391_3W/Repositories/IServiceRepository.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObj.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IServiceRepository
+    {
+        public Daservice FindServiceByName(string name);
+    }
+}
