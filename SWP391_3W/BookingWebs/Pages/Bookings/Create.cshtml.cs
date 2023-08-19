dcaec06c0074ddc9b966be80ec1ac983b631fc74
trungphamthanh <thnhtrungphm33@gmail.com>
2023-08-19 11:07:45 +0700

    upload Create

--- SWP391_3W/BookingWebs/Pages/Bookings/Create.cshtml.cs
+++ SWP391_3W/BookingWebs/Pages/Bookings/Create.cshtml.cs
@@ -5,34 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using BusinessObj.Models;
+using BusinessObj.Model;
 
 namespace BookingWebs.Pages.Bookings
 {
     public class CreateModel : PageModel
     {
-        private readonly BusinessObj.Models.DASContext _context;
+        private readonly BusinessObj.Model.DASContext _context;
 
-        public CreateModel(BusinessObj.Models.DASContext context)
+        public CreateModel(BusinessObj.Model.DASContext context)
         {
             _context = context;
         }
 
         public IActionResult OnGet()
         {
-        ViewData["AccountId"] = new SelectList(_context.Accounts, "Id", "Id");
-        ViewData["SlotId"] = new SelectList(_context.Slots, "Id", "Id");
+            ViewData["AccountId"] = new SelectList(_context.Accounts, "Id", "Id");
+            ViewData["SlotId"] = new SelectList(_context.Slots, "Id", "Id");
             return Page();
         }
 
         [BindProperty]
         public Booking Booking { get; set; } = default!;
-        
+
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.Bookings == null || Booking == null)
+            if (!ModelState.IsValid || _context.Bookings == null || Booking == null)
             {
                 return Page();
             }
